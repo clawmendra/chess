2bbdbdb219c8ab6aa1047a99b23bff0446b23e87
Almendra Clawson <mendrav@gmail.com>
2024-10-19 13:49:51 -0600

    Try added outline to Server.java for clear endpoint

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,13 +1,10 @@
 import chess.*;
 import server.Server;
-import com.google.gson.Gson;
-import spark.Spark;
 
 public class Main {
     public static void main(String[] args) {
-        int port = 8080;
         Server server = new Server();
-        server.run(port);
+        server.run(8080);
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
     }
---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,41 @@
+package server;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+import handler.ClearHandler;
+import service.ClearService;
+import spark.*;
+
+public class Server {
+    private final ClearHandler clearHandler;
+
+    public Server() {
+        DataAccess dataAccess = new MemoryDataAccess();
+        ClearService clearService = new ClearService(dataAccess);
+        this.clearHandler = new ClearHandler(clearService);
+    }
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+        Spark.delete('/db', (req, res) -> clearHandler.handle(req,res));
+
+       // Spark.exception(Exception.class, this::handleException);
+//      This line initializes the server and can be removed once you have a functioning endpoint
+//      Spark.init();
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+
+}
+
+
+
+
+
