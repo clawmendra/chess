0cd2423e4d31fac4b7b763f05687c8e142e1d678
Almendra Clawson <mendrav@gmail.com>
2024-10-19 14:59:11 -0600

    created RegisterService.java

---
+++ server/src/main/java/service/RegisterService.java
@@ -0,0 +1,32 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+
+import java.util.UUID;
+
+
+public class RegisterService {
+    private final DataAccess.dataAccess;
+
+    public RegisterService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+    public AuthData register(String username, String password, String email) throws DataAccessException {
+        if (username == null || password == null || email == null) {
+            throw new DataAccessException("Error: bad request");
+        }
+        if (dataAccess.getUser(username) != null) {
+            throw new DataAccessException("Error: already taken");
+        }
+
+        UserData user = new UserData(username, password, email);
+        dataAccess.createUser(user);
+
+        String authToken = UUID.randomUUID().toString();
+        AuthData auth = new AuthData(authToken, username);
+        dataAccess.createAuth(auth);
+
+        return auth;
+    }
+}
