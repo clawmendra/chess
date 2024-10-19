06919fd57be6fcc85d74cb87c709b05e1e13b756
Almendra Clawson <mendrav@gmail.com>
2024-10-19 11:53:17 -0600

    added test to see if port 8080 works

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,7 +1,13 @@
 import chess.*;
+import server.Server;
+import com.google.gson.Gson;
+import spark.Spark;
 
 public class Main {
     public static void main(String[] args) {
+        int port = 8080;
+        Server server = new Server();
+        server.run(port);
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
     }
