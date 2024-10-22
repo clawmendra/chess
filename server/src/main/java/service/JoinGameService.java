6a50060833d8239a52aa33bbcb7a8f7a8f6ff4f2
Almendra Clawson <mendrav@gmail.com>
2024-10-22 11:52:47 -0600

    Added JoinGameService

--- server/src/main/java/service/JoinGameService.java
+++ server/src/main/java/service/JoinGameService.java
@@ -15,10 +15,31 @@ public class JoinGameService {
             throw new DataAccessException("Error: unauthorized");
         }
         AuthData auth = dataAccess.getAuth(authToken);
-//        if (auth == null) {
-//            throw new DataAccessException
-//        }
+        if (auth == null) {
+            throw new DataAccessException("Error: bad request");
+        }
+        GameData game = dataAccess.getGame(gameID);
+        if (game == null) {
+            throw new DataAccessException("Error: bad request");
+        }
+        String username = auth.username();
+        GameData updateGame;
+
+        if (playercolor == null) {
+            updateGame = game;
+        }
+        else if (playercolor == ChessGame.TeamColor.WHITE) {
+            if (game.whitePlayer() != null) {
+                throw new DataAccessException("Error: already taken");
+            }
+            updateGame = new GameData(game.gameID(), username, game.blackPlayer(), game.gameName(), game.game());
+        }
+        else {
+            if (game.blackPlayer() != null) {
+                throw new DataAccessException("Error: already taken");
+            }
+            updateGame = new GameData(game.gameID(), game.whitePlayer(), username, game.gameName(), game.game());
+        }
+        dataAccess.updateGame(updateGame);
     }
-
-
 }
