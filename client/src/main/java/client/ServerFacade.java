e37c9f85a2ccf13f7e063aa1a30c5e782a25e711
Almendra Clawson <mendrav@gmail.com>
2024-11-11 16:46:51 -0700

    added register function and copied makeRequest from petshop

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,23 +1,50 @@
 package client;
 
 import com.google.gson.Gson;
-import exception.ResponseException;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.io.*;
+import java.net.*;
+
 
 public class ServerFacade {
-
     private final String serverUrl;
+    private final Gson gson;
 
     public ServerFacade(String url) {
         serverUrl = url;
         gson = new Gson();
     }
 
-    public RegisterResult register(RegisterRequest request){...}
-
-    public JoinResult join(JoinRequest request) {...}
-
+    public AuthData register(String username, String password, String email) throws Exception {
+        var userData = new UserData(username, password, email);
+        var path = "/user";
+        return this.makeRequest("POST", path, userData, AuthData.class);
     }
 
 
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+//    public JoinResult join(JoinRequest request) {...}
+//
+//    }
+
+}
 
 
