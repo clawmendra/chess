8cdaa019a64557680729e45ecd6997b86bf014da
Almendra Clawson <mendrav@gmail.com>
2024-11-12 11:43:16 -0700

    adjusted Chessclient to start running

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,4 +1,41 @@
 package client;
 
+import model.AuthData;
+import static ui.PreLogin.*;
+import java.util.Scanner;
+
 public class ChessClient {
+    private final ServerFacade server;
+    private State state = State.LOGGEDON;
+    private AuthData authData;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public enum State {
+        LOGGEDON,
+        LOGGEDOUT,
+    }
+
+    public ChessClient(String serverUrl) {
+        server = new ServerFacade(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("Welcome to 240 Chess. Type 'Help' to get started.");
+        while (true) {
+            try {
+                String line = scanner.nextLine();
+                if (state == State.LOGGEDOUT) {
+                    doPreLogin(line);
+                } else {
+                    doPostLogin(line);
+                }
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+
+    private void doPreLogin(String line) throws Exception {
+        return;
+    }
 }
