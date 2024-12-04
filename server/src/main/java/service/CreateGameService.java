b378052a2e1a76dea3a8bee9bc6ee8b6666d30de
Almendra Clawson <mendrav@gmail.com>
2024-12-04 14:46:57 -0700

    fixed 400 bad request error with creating games

--- server/src/main/java/service/CreateGameService.java
+++ server/src/main/java/service/CreateGameService.java
@@ -5,11 +5,26 @@ import dataaccess.DataAccessException;
 import model.AuthData;
 import model.GameData;
 
+import javax.xml.crypto.Data;
+
 public class CreateGameService {
     private final DataAccess dataAccess;
     private static int newGameID = 1;
     public CreateGameService(DataAccess dataAccess) {
         this.dataAccess = dataAccess;
+        try {
+            GameData[] games = dataAccess.listGames();
+            int maxGameID = 0;
+            for (GameData game : games) {
+                if (game.gameID() > maxGameID) {
+                    maxGameID = game.gameID();
+                }
+            }
+            // Set newGameID to one more than the highest existing ID
+            newGameID = maxGameID + 1;
+        } catch (DataAccessException e) {
+            newGameID = 1000;
+    }
     }
     public int createGame(String authToken, String gameName) throws DataAccessException {
         if (authToken == null || gameName == null) {
